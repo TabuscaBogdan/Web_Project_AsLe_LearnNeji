24f060449b952513fae7b39873f1adfa89bd6852
Bogdan <bogdan.tabusca@gmail.com>
2017-11-27 17:46:45 +0200

    <Simbols & repairs> +added current simbol image + script to change the image depending on     the selected simbol  -some adjustments

---
+++ Scripts/Selectors.js
@@ -0,0 +1,17 @@
+function get_selected_lang(language)
+{
+    var nume=language.text;
+    var path="../Images/Simbols/"+nume;
+    //nu poti itera prin foldere...
+    //need more info
+}
+
+
+function get_selected_symbol(simbol)
+{
+    var nume=simbol.text.split("-");
+    //window.alert(nume[1]);
+    //trebuie adaugat si pt limbi
+    var path="../Images/Simbols/Japanese/hiragana-"+nume[1]+".png";
+    document.getElementById("current_symbol").src=path;
+}
