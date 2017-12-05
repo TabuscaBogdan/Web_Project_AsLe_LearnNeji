0dd3c98f29ee40eb9cbe91f4de12df3a2bb04c52
Bogdan <bogdan.tabusca@gmail.com>
2017-12-05 14:19:34 +0200

    <Image Comparation 2.0 IMPROVED!> +++It NO LONGER TAKES 5 min for image comparation +Fixed Img Comparation Bugs + Added Simbol Completion Precent - still has some bugs

--- Scripts/Matchers.js
+++ Scripts/Matchers.js
@@ -30,14 +30,21 @@ function get_line_intervals(canvas_context,c_height,c_width)
     var found=0;
     var a,b;
     var intervale=[];
+
+    var pixels = canvas_context.getImageData(0, 0, c_width, c_height);
+    var blueComponent;
+
+    console.time('someFunction');
     for( var linie=0;linie<c_height;linie++)
     {
         for(var j=0;j<c_width;j++)
         {
-            var pixel = canvas_context.getImageData(linie, j, 1, 1);
+            // var pixel = canvas_context.getImageData(linie, j, 1, 1);
+            blueComponent = pixels.data[((linie * (pixels.width * 4)) + (j * 4)) ];
+
             if(found==0)
             {
-                if(pixel.data[0]==0)
+                if(blueComponent==0)
                 {
                     found=1;
                     a=j;
@@ -45,7 +52,7 @@ function get_line_intervals(canvas_context,c_height,c_width)
             }
             else
             {
-                if(pixel.data[0]==255)
+                if(blueComponent==255)
                 {
                     found=0;
                     b=j;
@@ -55,39 +62,45 @@ function get_line_intervals(canvas_context,c_height,c_width)
             }
         }
     }
+
+    console.timeEnd('someFunction');
+
+    console.log(intervale)
     return intervale;
 }
 
 function compare_intervals(inta,intb)
 {
-    if(inta["st"]>=intb["st"])
+    if(inta["st"]>intb["st"])
     {
-        if(inta["end"]<=intb["end"])
+        if(inta["end"]<intb["end"])
         {
-            return {line:0,st:0,end:0}
+            return [{line:inta["line"],st:intb["st"],end:inta["st"]},{line:inta["line"],st:inta["end"],end:intb["end"]}]
         }
         else
-            if(inta["st"]<=intb["st"])
-                return {line:inta["line"],st:intb["end"],end:inta["end"]}
+            if(inta["st"]<=intb["end"])
+                return [{line:inta["line"],st:intb["end"],end:inta["end"]}]
             else
-                return inta;
+                return [inta];
     }
     else
     {
         if(inta["end"]<=intb["st"])
         {
-            return inta;
+            return [inta];
         }
         else
         {
             if(inta["end"]>intb["end"])
-                return intb;
+                return [{line:inta["line"],st:inta["st"],end:intb["st"]},{line:inta["line"],st:intb["end"],end:inta["end"]}]
             else
-                return {line:inta["line"],st:inta["st"],end:intb["st"]}
+                return [{line:inta["line"],st:inta["st"],end:intb["st"]}]
         }
     }
+
 }
 
+//pt gasirea intervalelor de pe aceeasi linie
 function get_interval_chunks(draw,simb,numberDr,numberSi,start)
 {
     var i=start;
@@ -116,6 +129,8 @@ function get_interval_chunks(draw,simb,numberDr,numberSi,start)
     return [si,dr]
 }
 
+//pt compararea efectiva de intervale
+//returneaza intervalele care vor fi colorate in rosu (DE PE LINIA DATA)
 function bad_intervals(chunks)
 {
     var i=0,j=0;
@@ -126,8 +141,8 @@ function bad_intervals(chunks)
     while (i<si_len&&j<dr_len)
     {
         r_interval=compare_intervals(chunks[1][j],chunks[0][i]);
-        if(r_interval["line"]!=0)
-            bad.push(r_interval);
+        if(r_interval[0]["line"]!=0)
+            bad.push.apply(bad,r_interval);
         i++;
         j++;
     }
@@ -151,6 +166,7 @@ function bad_intervals(chunks)
 
 }
 
+// returneaza toate intervalele ce trebuie colorate cu rosu
 function compare_pictures()
 {
     var info=prepare();
@@ -162,15 +178,44 @@ function compare_pictures()
     var chunk;
 
     var bad_intervales=[];
-    for(var linie=simbol[0]["line"];linie<=si_len;linie++)
+    for(var linie=simbol[0]["line"];linie<si_len;linie++)
     {
         chunk=get_interval_chunks(drawing,simbol,dr_len,si_len,linie);
         linie=linie+chunk[0].length;
         bad_intervales.push.apply(bad_intervales,bad_intervals(chunk));
     }
-    return bad_intervales;
+
+    //resturile
+    var j=0;
+    while (drawing[j]["line"]<simbol[0]["line"]&& j<dr_len)
+    {
+        bad_intervales.push(drawing[j])
+        j++;
+    }
+    j=dr_len-1;
+    si_len--;
+    while(drawing[j]["line"]>simbol[si_len]["line"]&&j>0)
+    {
+        bad_intervales.push(drawing[j])
+        j--;
+    }
+    //pt statistica
+    var good_nr=0;
+    var err_nr=0;
+    for(var i=0;i<si_len;i++)
+    {
+        good_nr=good_nr+(simbol[i]["end"]-simbol[i]["st"]);
+    }
+    var err_len=bad_intervales.length;
+    for(var i=0;i<err_len;i++)
+    {
+        err_nr=err_nr+(bad_intervales[i]["end"]-bad_intervales[i]["st"]);
+    }
+
+    return [bad_intervales,good_nr,err_nr];
 }
 
+//compararea efectiva a imaginilor
 function expose_errors() {
      //WIP
     var cv=document.getElementById("can");
@@ -196,15 +241,15 @@ function expose_errors() {
 
     var errors=compare_pictures();
 
-    var m_errors=errors.length;
+    var m_errors=errors[0].length;
     var linie=0;
     var j=0;
 
     fcv.fillStyle="red";
     for (i=0;i<m_errors;i++)
     {
-        linie=errors[i]["line"];
-        fcv.fillRect(linie,errors[i]["st"],1,errors[i]["end"]-errors[i]["st"]);
+        linie=errors[0][i]["line"];
+        fcv.fillRect(errors[0][i]["st"],linie,errors[0][i]["end"]-errors[0][i]["st"],1);
         /*for(j=errors[i]["st"];j<=errors[i]["end"];j++)
         {
             pix=fcv.getImageData(linie, j, 1, 1);
@@ -219,8 +264,16 @@ function expose_errors() {
     }
     document.getElementById("canvasimg").src = finale.toDataURL("image/png");
     document.getElementById("canvasimg").style.display = "inline";
+
+    if(errors[2]>errors[1])
+    {
+        errors[2]=-errors[2];
+    }
+    var precent_good=(100.0*errors[2])/errors[1];
+    window.alert(precent_good+'%');
 }
 
+//for testing pourpouses
 function test()
 {
     var t=compare_pictures();
