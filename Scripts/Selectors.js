5414cdcd3d0583deb743176a99d6958a07ad8769
Bogdan <bogdan.tabusca@gmail.com>
2017-12-12 11:11:56 +0200

    <Flashy Bits> +integrated gif images to help the learning process +minor bug fixes

--- Scripts/Selectors.js
+++ Scripts/Selectors.js
@@ -12,6 +12,20 @@ function get_selected_symbol(simbol)
     var nume=simbol.text.split("-");
     //window.alert(nume[1]);
     //trebuie adaugat si pt limbi
-    var path="../Images/Simbols/Japanese/h-"+nume[1]+".gif";
+    var path="../Images/Simbols/Japanese/hiragana-"+nume[1]+".png";
     document.getElementById("current_symbol").src=path;
+}
+
+function hover(element) {
+    //window.alert(element.src)
+    var nume=element.src.split("-");
+    var tname=nume[1].split(".");
+    element.src="../Images/Simbols/Japanese/h-"+tname[0]+".gif";
+    //element.setAttribute('src', "LearnNeji/Simbols/Images/Simbols/Japanse/h-"+tname[0]+".gif");
+    //window.alert(element.src)
+}
+function unhover(element) {
+    var nume=element.src.split("-");
+    var tname=nume[1].split(".");
+    element.setAttribute('src', "../Images/Simbols/Japanese/hiragana-"+tname[0]+".png");
 }
